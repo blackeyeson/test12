f5e4c0dc8844edb3b07c7823de8535cd1a6477b5
davit shavtvalishvili <dttshavtvalishvili@gmail.com>
2022-07-01 22:16:57 +0400

    Initial Commit

---
+++ test12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test12
+//
+//  Created by დავით შავთვალიშვილი on 01.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

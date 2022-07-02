4421cca31fe4b7b59921732d7b075fca4821aae9
davit shavtvalishvili <dttshavtvalishvili@gmail.com>
2022-07-02 16:09:21 +0400

    updated

--- test12/ViewController.swift
+++ test12/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    func trueRandomColor() -> UIColor {
+        UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
+    }
+    func randomColor() -> UIColor {
+        let colors: [UIColor] = [.red, .blue, . green]
+        return colors.randomElement() ?? .brown
+    }
+    func checkColors() {
+        if label1.backgroundColor == view.backgroundColor {
+            label1.text = "colors are thesame"
+        } else { label1.text = "colors are different" }
+    }
+    
+    @IBAction func changeBackground(_ sender: Any) {
+        self.view.backgroundColor = randomColor()
+        checkColors()
+        
+    }
+    @IBAction func changeLabelBackground(_ sender: Any) {
+        label1.backgroundColor = randomColor()
+        checkColors()
+    }
 }
 
